150878c11e4f180673fd5ec7405dd3acbd0ca4a7
horyunsong <horyunsong@gmail.com>
2017-04-15 12:37:08 -0400

    testData

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Header from './Header';
+
+
+class App extends React.Component {
+  state = {
+    pageHeader: 'Naming Contests!!'
+  };
+  componentDidMount() {
+    //timers, listeners
+  }
+  componentWillUnMount() {
+    //clean timers, listeners
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header message={this.state.pageHeader} />
+        <div>
+          {this.state.test}
+        </div>
+      </div>
+    );
+  }
+};
+
+export default App;
